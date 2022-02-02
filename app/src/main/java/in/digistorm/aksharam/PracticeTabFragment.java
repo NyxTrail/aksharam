e0611181583da4ab3d80c2fa47530dcbf17943fa
alan <alan@digistorm.in>
2022-02-02 11:37:55 +0530

    PracticeTabFragment: Begin dev
    - initial commit, layout file added

---
+++ app/src/main/java/in/digistorm/aksharam/PracticeTabFragment.java
@@ -0,0 +1,27 @@
+package in.digistorm.aksharam;
+
+/*
+ * Copyright (c) 2022 Alan M Varghese <alan@digistorm.in>
+ *
+ * This files is part of Aksharam, a script teaching app for Indic
+ * languages.
+ *
+ * Aksharam is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Aksharam is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even teh implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import androidx.fragment.app.Fragment;
+
+public class PracticeTabFragment extends Fragment {
+
+}
