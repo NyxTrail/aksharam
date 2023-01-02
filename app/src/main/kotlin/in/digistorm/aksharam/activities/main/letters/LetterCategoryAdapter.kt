0508768fc6236b16495fd288f2cbcd650d4ebb9a
Alan M Varghese <alan@digistorm.in>
2023-01-02 23:28:47 +0530

    Animated Card Views
    - Collapse and Expand animation added for Card Views
    - Collapse animation looks ugly when there are hidden items in the
      recycler view

--- app/src/main/kotlin/in/digistorm/aksharam/activities/main/letters/LetterCategoryAdapter.kt
+++ app/src/main/kotlin/in/digistorm/aksharam/activities/main/letters/LetterCategoryAdapter.kt
@@ -1,18 +1,33 @@
+/*
+ * Copyright (c) 2022 Alan M Varghese <alan@digistorm.in>
+ *
+ * This files is part of Aksharam, a script teaching app for Indic
+ * languages.
+ *
+ * Aksharam is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Aksharam is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even teh implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package `in`.digistorm.aksharam.activities.main.letters
 
 import `in`.digistorm.aksharam.R
 import `in`.digistorm.aksharam.activities.main.MainActivity
+import `in`.digistorm.aksharam.util.ExpandableCardView
 import `in`.digistorm.aksharam.util.Transliterator
 import `in`.digistorm.aksharam.util.logDebug
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.gridlayout.widget.GridLayout
-import android.widget.LinearLayout
-import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.children
-import androidx.core.view.size
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -38,6 +53,15 @@ class LetterCategoryAdapter(
         lettersCategoryWise = letters
     }
 
+    private fun findNextCategory(category: String): String? {
+        logDebug(logTag, "Finding next category of $category")
+        val indexOfCategory = categories.indexOf(category)
+        if(indexOfCategory + 1 == categories.size)
+            return null
+        logDebug(logTag, "Next category: ${categories[indexOfCategory + 1]}")
+        return categories[indexOfCategory + 1]
+    }
+
     private class LetterCategoryDiff: DiffUtil.ItemCallback<String>() {
         private val logTag = this.javaClass.simpleName
         override fun areItemsTheSame(
@@ -60,27 +84,48 @@ class LetterCategoryAdapter(
         }
     }
 
-    class LetterCategoryCardViewHolder(cardView: View): RecyclerView.ViewHolder(cardView) {
-        val categoryHeader: ConstraintLayout
-        val letterCategoryHeaderText: TextView
-        val letterGrid: GridLayout
-
+    class LetterCategoryCardViewHolder(
+        val cardView: ExpandableCardView
+    ): RecyclerView.ViewHolder(cardView) {
         init {
-            categoryHeader = cardView.findViewById(R.id.letter_category_header)
-            letterCategoryHeaderText = cardView.findViewById(R.id.letter_category_header_text)
-            letterGrid = cardView.findViewById(R.id.letter_grid)
+            cardView.initialize()
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LetterCategoryCardViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.letter_category, parent, false)
-        return LetterCategoryCardViewHolder(view)
+        return LetterCategoryCardViewHolder(view as ExpandableCardView)
     }
 
     override fun onBindViewHolder(holder: LetterCategoryCardViewHolder, position: Int) {
         logDebug(logTag, "Binding view for position: $position")
         logDebug(logTag, "Items: $currentList")
-        holder.letterCategoryHeaderText.text = getItem(position).replaceFirstChar {
+        holder.cardView.tag = getItem(position)
+
+        // Give an ExpandableCardView the ability to find its sibling
+        holder.cardView.findSiblings = fun () : MutableList<ExpandableCardView> {
+            var nextCategory = findNextCategory(getItem(position))
+            val siblingList = mutableListOf<ExpandableCardView>()
+            var cardView: ExpandableCardView? = null
+            // Collect all Card Views sequentially, until the first un-collapsed one
+            while(nextCategory != null) {
+                logDebug(logTag, "In loop")
+                cardView = (holder.cardView.parent as View).findViewWithTag(nextCategory)
+                        ?: break // We couldn't find the next category
+                if(cardView.collapsed)
+                    siblingList.add(cardView)
+                else
+                    break // We break at the first Card View that is not collapsed
+                // Next category
+                nextCategory = findNextCategory(nextCategory)
+            }
+            if(cardView != null && !cardView.collapsed)
+                siblingList.add(cardView)
+
+            return siblingList
+        }
+
+        holder.cardView.letterCategoryHeaderTextView.text = getItem(position).replaceFirstChar {
             if(it.isLowerCase())
                 it.titlecase()
             else
@@ -96,14 +141,20 @@ class LetterCategoryAdapter(
         letterCategoryCardViewHolder: LetterCategoryCardViewHolder,
         position: Int,
     ) {
-        letterCategoryCardViewHolder.letterGrid.apply {
+        letterCategoryCardViewHolder.cardView.letterGrid.apply {
             removeAllViews()
 
+            logDebug(logTag, "position to get: $position")
+            logDebug(logTag, "Categories are: ")
+            categories.forEach {
+                logDebug(logTag, it)
+            }
+            logDebug(logTag, "categories[position]: ${categories[position]}")
             for(letter in lettersCategoryWise[categories[position]]!!) {
                 val letterView: LetterView = LayoutInflater
-                    .from(letterCategoryCardViewHolder.letterGrid.context)
+                    .from(letterCategoryCardViewHolder.cardView.letterGrid.context)
                     .inflate(R.layout.letter_view,
-                        letterCategoryCardViewHolder.letterGrid, false) as LetterView
+                        letterCategoryCardViewHolder.cardView.letterGrid, false) as LetterView
                 letterView.setOnLongClickListener {
                     logDebug(logTag, "$letter long clicked!")
                     val letterInfoFragment = LetterInfoFragment(
@@ -115,7 +166,7 @@ class LetterCategoryAdapter(
                     true
                 }
                 letterView.letters = Pair(letter, transliterator.transliterate(letter, targetLanguage))
-                letterCategoryCardViewHolder.letterGrid.addView(letterView)
+                letterCategoryCardViewHolder.cardView.letterGrid.addView(letterView)
             }
         }
     }
@@ -140,7 +191,7 @@ class LetterCategoryAdapter(
                     Pair(it, transliterator.transliterate(it, targetLanguage))
                 }.let {
                         logDebug(logTag, "List contains: $it")
-                        letterGrid.children.forEachIndexed { i, view ->
+                        cardView.letterGrid.children.forEachIndexed { i, view ->
                             (view as LetterView).letters = it[i]
                         }
                     }
